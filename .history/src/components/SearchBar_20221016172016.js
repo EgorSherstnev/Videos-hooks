6d25a58a162cba344072e090dd2c5f6683b8cfae
EgorSherstnev <svarog91@list.ru>
2022-10-17 22:32:18 +0300

    added inside the component SearchBar.js methods

--- .history/src/components/SearchBar_20221016172016.js
+++ .history/src/components/SearchBar_20221016172016.js
@@ -3,21 +3,14 @@ import React from "react";
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onInputChange = (event) => {
-        this.setState({ term: event.target.value });
-    };
+    onInputChange = () => {
 
-    onFormSubmit = (event) => {
-        event.preventDefault();
-
-        //TODO: Make sure we call
-        //callback from parent component
     }
 
     render () {
         return (
             <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
